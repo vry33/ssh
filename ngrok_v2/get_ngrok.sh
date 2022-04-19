423869befb99dc7ce41b10b0529065503901ca21
vry33 <103950607+vry33@users.noreply.github.com>
2022-04-19 11:43:43 +0700

    Update get_ngrok.sh

--- ngrok_v2/get_ngrok.sh
+++ ngrok_v2/get_ngrok.sh
@@ -4,5 +4,4 @@ echo "Download ngrok"
 echo "======================="
 wget -q -c -nc -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip -qq -n ngrok.zip > /dev/null 2>&1
-read -p "Paste Ngrok Authtoken: " CRP
-./ngrok authtoken $CRP
+./ngrok authtoken $1
