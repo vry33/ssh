8ef3ac971b6773a425850eef86819f9a9495fa9a
vry33 <103950607+vry33@users.noreply.github.com>
2022-04-19 11:37:23 +0700

    Update main.sh

--- ngrok_v2/main.sh
+++ ngrok_v2/main.sh
@@ -23,6 +23,5 @@ echo "export LD_LIBRARY_PATH" >> /root/.bashrc
 url=$(curl "http://localhost:4040/api/tunnels" | jq '.tunnels[0].public_url' | cut -d'"' -f2)
 host=$(echo $url | sed 's\:[0-9].*\\g' | sed 's\tcp://\\g')
 port=$(echo $url | sed 's\.*io:\\g')
-echo "ssh -p${port} root@${host}
-
+echo "ssh -p${port} root@${host}"
 echo $insta
